debfe3142a2aa5b8d6299cf3a3afd741f4932f2e
Ashur Cabrera <hello@ashur.cab>
2021-05-30 21:38:38 -0600

    Improve logging

--- functions/middleware/auth.js
+++ functions/middleware/auth.js
@@ -1,10 +1,14 @@
 module.exports = (req, res, next) =>
 {
-	console.log( req.headers );
-	console.log( process.env );
-
 	if( req.headers["authorization"] !== process.env.APP_ADMIN_TOKEN )
 	{
+		console.log( "Failed login attempt:", {
+			authorization: req.headers["authorization"],
+			client: req.headers["client-ip"],
+			agent: req.headers["user-agent"],
+			resource: `${req.method} ${req.url}`,
+		} );
+
 		res.status( 401 );
 		res.json({
 			title: "Unauthorized",
